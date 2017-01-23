315141f4523b17848ca3f1b6cb6e97a82aaa12a5
Enoch2k2 <enoch2k2@hotmail.com>
2017-01-23 15:57:36 -0600

    Done.

--- src/containers/PetsShow.js
+++ src/containers/PetsShow.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-export default () => {
-
+const PetsShow = (props) => {
+  // console.log(props.pet)
+  // props.routeParams : id
+  // need to find a way to parse the pets
   return (
     <div className="col-md-8">
+      <h2>{props.pet.name}</h2>
+      <p>{props.pet.description}</p>
     </div>)
 }
+
+function mapStateToProps(state, ownProps){
+  const pet = state.pets.find((pet) => pet.id == ownProps.routeParams.id)
+
+  if(pet){
+    return {pet: pet}
+  } else {
+    return {pet: {}}
+  }
+}
+
+export default connect(mapStateToProps)(PetsShow)
